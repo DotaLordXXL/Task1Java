4cc29feabf27d5ac2e08611606dc5199f2a3e58e
DotaLordXXL <78918946+DotaLordXXL@users.noreply.github.com>
2021-03-15 15:16:16 +0300

    Add files via upload

---
+++ Task1.java
@@ -0,0 +1,59 @@
+public class Task1{
+	public static void main(String[] args) {
+		System.out.println(convert(6));
+		System.out.println(points(15, 6));
+		System.out.println(footballPoints(15, 6, 4));
+		System.out.println(divisableByFive(15));
+		System.out.println(and(false, true));
+		System.out.println(howManyWalls(46, 5, 4));
+		System.out.println(squaed(5));
+		System.out.println(profitableGamble(0.9, 3, 2));
+		System.out.println(frames(10, 25));
+		System.out.println(mod(25, 3));
+	}
+	public static int convert(int minute) {
+		return minute * 60;
+	}
+	public static int points(int x2, int x3) {
+		return x2 * 2 + x3 * 3;
+	}
+	public static int footballPoints(int win, int draw, int loss) {
+		return win * 3 + draw + loss * 0;
+	}
+	public static boolean divisableByFive(int num) {
+		if(num % 5 == 0) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	public static boolean and(boolean a, boolean b) {
+		if(a && b) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	public static int howManyWalls(int n, int w, int h) {
+		return n / (w * h) ;
+	}
+	public static int squaed(int b) {
+		return b * b;
+		}
+	public static boolean profitableGamble(double prob, int prize, int pay) {
+		if(prob * prize > pay) {
+			return true;
+		}
+		else {
+			return false;
+		}
+		}
+	public static int frames(int fps, int min) {
+		return fps * 60 * min;
+	}
+	public static int mod(int num, int del) {
+		return num - (num / del * del);
+	}
+}
